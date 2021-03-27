41dd01b7c060f8716bc562f4bad1de16d71570f0
VeronicaBlake <vrex149@gmail.com>
2021-03-26 21:53:39 -0600

    added plusOne and played with auto

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 let feed = 0;
 let modifiers = 1;
-let autoMod = 1;
+let plusMod = 0;
+let boxMod = 1;
+let bakerMod = 1;
 
 let clickUpgrades = {
     Double:{
-        price: 1,
+        price: 2,
         quantity: 0,
         multiplier: 2
     },
+    plusMod:{
+        price: 2,
+        quantity: 0,
+        multiplier: 2
+    }
 };
 
 let autoUpgrades = {
     box:{
         price: 5,
         quantity: 0,
-        multiplier: 12
+        multiplier: 5
     },
     baker:{
         price: 10,
         quantity: 0,
-        multiplier: 24
+        multiplier: 10
     }
 }
 
@@ -27,9 +34,20 @@ function buyDouble(){
     if (feed >= clickUpgrades.Double.price){
         (feed -= clickUpgrades.Double.price)
         clickUpgrades.Double.quantity ++
-        document.getElementById('double').innerText = clickUpgrades.Double.quantity
         modifiers = modifiers* clickUpgrades.Double.multiplier 
+        document.getElementById('double').innerText = clickUpgrades.Double.quantity
         clickUpgrades.Double.price = clickUpgrades.Double.price*modifiers
+        update()
+    }
+}
+
+function plusOne(){
+    if (feed >= clickUpgrades.plusMod.price){
+        (feed -= clickUpgrades.plusMod.price)
+        clickUpgrades.plusMod.quantity ++
+        plusMod = plusMod+clickUpgrades.plusMod.quantity 
+        document.getElementById('plusOne').innerText = clickUpgrades.plusMod.quantity
+        clickUpgrades.plusMod.price = clickUpgrades.plusMod.price*plusMod
         update()
     }
 }
@@ -38,7 +56,7 @@ function buyBox(){
     if (feed >= autoUpgrades.box.price){
         feed -= autoUpgrades.box.price
         autoUpgrades.box.quantity ++
-        autoMod = modifiers*autoUpgrades.box.multiplier
+        boxMod = boxMod*autoUpgrades.box.multiplier
         document.getElementById('box').innerText = autoUpgrades.box.quantity
         startInterval()
         update()
@@ -49,6 +67,7 @@ function buyBaker(){
     if (feed >= autoUpgrades.baker.price){
         feed -= autoUpgrades.baker.price
         autoUpgrades.baker.quantity ++
+        bakerMod = bakerMod*autoUpgrades.baker.multiplier
         document.getElementById('baker').innerText = autoUpgrades.baker.quantity
         startInterval()
         update()
@@ -56,7 +75,7 @@ function buyBaker(){
 }
 
 function collectAutoUpgrades(){
-    feed = autoMod + feed
+    feed = boxMod + feed + bakerMod
     update()
 }
 
@@ -65,10 +84,10 @@ function startInterval() {
 }
 
 function mine(){
-    feed = modifiers + feed
+    feed = modifiers + feed + plusMod
     update()
 }
 
 function update(){
-    document.getElementById('fed').innerText = feed
+    document.getElementById('feed').innerText = feed
 }
